43ff64ac08876b01ba7f68a05eee8613b8d7bc2d
Rafael <rafael.aguiar91@gmail.com>
2023-05-17 19:09:53 -0300

    ex 3 resolvido

--- index.ts
+++ index.ts
@@ -35,3 +35,32 @@ const cadastrarUsuario = (usuario: Usuario): Usuario =>{
 const listagemUsuarios = ():Usuario[] => {
     return lerArquivo() as Usuario[];
 }
+
+const detalharUsuario = (cpf:string):Usuario => {
+    const bd = lerArquivo() as Usuario[];
+    const usuario = bd.find(usuario => {
+        return usuario.cpf === cpf; 
+    })
+
+    if(!usuario){
+        throw new Error('Usuário não encontrado');
+    }
+
+    return usuario;
+}
+
+const atualizarUsuario = (cpf:string, dados:Usuario):Usuario => {
+    const bd = lerArquivo() as Usuario[];
+    const usuario = bd.find(usuario => {
+        return usuario.cpf === cpf; 
+    })
+    if(!usuario){
+        throw new Error('Usuário não encontrado');
+    }
+    Object.assign(usuario, dados)
+    escreverArquivo(bd);
+
+    return dados
+}
+
+
